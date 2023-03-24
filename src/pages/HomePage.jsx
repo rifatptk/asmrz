06ca28ad86e1d69aed80ecd801604367b76987d1
rifatptk <rifatptk@gmail.com>
2023-03-24 11:03:00 +0600

    add magical text section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FileTree from "../components/fileTree";
 import HoverBanner from "../components/HoverBanner";
+import MagicalText from "../components/magicalText/magicalText";
 
 export default function HomePage() {
   return (
     <div id="home-page">
       <HoverBanner />
       <FileTree />
+      <MagicalText />
     </div>
   );
 }
